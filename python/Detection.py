1f1413a1386853523c64e0f1326d6e95c82d88e2
Conrad Li <36898399+conradli@users.noreply.github.com>
2019-08-28 16:04:24 -0400

    Relative path set up
    Allows for easy set up on individual local environment rather than one computer

--- python/Detection.py
+++ python/Detection.py
@@ -1,57 +1,92 @@
-
 # import the necessary packages
-
+import argparse
 import cv2
 import os # path
 
-# Reading the image as it is
-image = cv2.imread ("/Users/ravipatel/Desktop/val2017/2017_30622680.jpg",1)
+if __name__=="__main__":
+    # local python directory path
+    python_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # local project directory path
+    project_dir = os.path.dirname(python_dir)
+    
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,
+                    help="path to input image")
+    
+    args = vars(ap.parse_args())
+    
+    # Reading the image as it is
+    image = cv2.imread(args["image"],1)
 
-# dimension array
-dimensions = image.shape
-height = image.shape[0]
-width = image.shape[1]
-channels = image.shape[2]
+    # dimension array
+    dimensions = image.shape
+    height = image.shape[0]
+    width = image.shape[1]
+    channels = image.shape[2]
+    /**
+    print('Image Dimesion    : ',dimensions)
+    print('Image Height       : ',height)
+    print('Image Width        : ',width)
+    print('Number of Channels : ',channels)
+    */
 
+    if (width < 1100):
+        scaleFactor =1.3
 
-print('Image Dimension    : ',dimensions)
-print('Image Height       : ',height)
-print('Image Width        : ',width)
-print('Number of Channels : ',channels)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-if (width < 1100):
-    scaleFactor =1.3
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.5,
+        minNeighbors= 3,
+        minSize=(50, 50)
+    )
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    print("[INFO] Found {0} Faces.".format(len(faces)))
+    
+    # create directory to store extracted faces
+    try:
+        os.makedirs("FacesExtracted")
+    except FileExistsError:
+        pass
+    
+    # create directory to store group photo 
+    try:
+        os.makedirs("groupPhoto")
+    except FileExistsError:
+        pass
+    
+     # Store predicted faces in this directory
+    store_faces_path = os.path.join(project_dir, 'FacesExtracted')
+    # Store the group photo in this directory
+    store_image_path = os.path.join(project_dir, 'groupPhoto')
+    
+    image_directory = os.listdir(store_image_path)
+    
+    # keep track of image number so faces can be associated with image
+    image_num = len(image_directory) + 1
+    
+    # counter to keep track of number of faces detected in photo
+    face_counter = 0
 
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.5,
-    minNeighbors= 3,
-    minSize=(50, 50)
-)
+    for (x, y, w, h) in faces:
+        roi_color = image[y:y + h, x:x + w]
+        cv2.imwrite(os.path.join(path, str(x) + str(y) + "_faces.jpg"), roi_color)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0,255,0), 14)
 
-print("[INFO] Found {0} Faces.".format(len(faces)))
+        print("[INFO] Object found. Saving locally.")
 
-#path for images extracted that needs to be stored
-path = '/Users/ravipatel/Desktop/Extract'
+     # resizes the output to 900x900 image
+    resize_image = cv2.resize(image, (900, 900))
+    
+    image_name = "image" + str(image_num) + ".jpg"
 
-for (x, y, w, h) in faces:
-    roi_color = image[y:y + h, x:x + w]
-    cv2.imwrite(os.path.join(path, str(x) + str(y) + "_faces.jpg"), roi_color)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0,255,0), 14)
+    status = cv2.imwrite(os.path.join(store_image_path,image_name), resize_image)
 
-    print("[INFO] Object found. Saving locally.")
+    print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
 
-#group photo showing detection
-extract = '/Users/ravipatel/Desktop/Group'
-
-status = cv2.imwrite(os.path.join(extract,'faces_detected.jpg'), image)
-
-print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
-cv2.imshow("Output",image)
-
-
-cv2.waitKey(10000)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
