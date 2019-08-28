593ea5174d6c83282995743488817157512cae34
Conrad Li <36898399+conradli@users.noreply.github.com>
2019-08-28 16:09:39 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var multipleUploadForm = document.querySelector('#multipleUploadForm');
+var multipleFileUploadInput = document.querySelector('#multipleFileUploadInput');
+var multipleFileUploadError = document.querySelector('#multipleFileUploadError');
+var multipleFileUploadSuccess = document.querySelector('#multipleFileUploadSuccess');
+
+
+function uploadMultipleFiles(files) {
+    var formData = new FormData();
+    for(var index = 0; index < files.length; index++) {
+        formData.append("files", files[index]);
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/uploadMultipleFiles");
+
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+        var response = JSON.parse(xhr.responseText);
+        if(xhr.status == 200) {
+            multipleFileUploadError.style.display = "none";
+			var content = "<p>All Files Uploaded Successfully</p>";
+            multipleFileUploadSuccess.innerHTML = content;
+            multipleFileUploadSuccess.style.display = "block";
+        } else {
+            multipleFileUploadSuccess.style.display = "none";
+            multipleFileUploadError.innerHTML = (response && response.message) || "Some Error Occurred";
+        }
+    }
+    xhr.send(formData);
+}
+
+multipleUploadForm.addEventListener('submit', function(event){
+    var files = multipleFileUploadInput.files;
+    if(files.length === 0) {
+        multipleFileUploadError.innerHTML = "Please select at least one file";
+        multipleFileUploadError.style.display = "block";
+    }
+    uploadMultipleFiles(files);
+    event.preventDefault();
+}, true);
+
+
+
+
+function previewImages() {
+  var preview = document.querySelector('#preview');
+  
+  if (this.files) {
+    [].forEach.call(this.files, readAndPreview);
+  }
+
+  function readAndPreview(file) {
+    var reader = new FileReader();
+    
+    reader.addEventListener("load", function() {
+      var image = new Image(200, 200);
+      image.title  = file.name;
+      image.src    = this.result;
+	  preview.appendChild(image);
+	  preview.innerHTML += '&nbsp;&nbsp;&nbsp;';
+	  preview.innerHTML += '<br>'
+    });
+    
+    reader.readAsDataURL(file);  
+  }
+}
+document.querySelector('#multipleFileUploadInput').addEventListener("change", previewImages);
+
